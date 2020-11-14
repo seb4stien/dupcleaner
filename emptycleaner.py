8e07c6b7d5cebb41a7c4d8e0a5745698ed120a52
Sébastien Georget <sebastien.georget@adevinta.com>
2020-11-14 15:11:13 +0100

    feat: can now delete remaining files like Thumbs.db

--- emptycleaner.py
+++ emptycleaner.py
@@ -14,11 +14,25 @@ import click
 
 @click.command()
 @click.option("-p", "--path", required=True, type=Path, help="Path to clean")
-def cli(path: Path):
+@click.option(
+    "-d",
+    "--delete",
+    multiple=True,
+    type=str,
+    help="Files that can be deleted if they are alone in a folder.",
+)
+def cli(path: Path, delete: str):
     for dirpath, dirnames, filenames in os.walk(path.as_posix(), topdown=False):
-        if not dirnames and not filenames:
-            click.secho(f"Removing {dirpath}")
-            os.rmdir(dirpath)
+        if not dirnames:
+            if len(filenames) == 1 and filenames[0] in delete:
+                to_delete = os.path.join(dirpath, filenames[0])
+                click.secho(f"Removing {to_delete}", fg="yellow")
+                os.unlink(to_delete)
+                filenames.pop()
+
+            if not filenames:
+                click.secho(f"Removing {dirpath}")
+                os.rmdir(dirpath)
 
 
 if __name__ == "__main__":
